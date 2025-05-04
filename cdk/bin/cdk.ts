72a89b580a47c4d41c86f932e854c275ff61e086
geghamhak <hak.gegh@gmail.com>
2025-05-04 17:08:47 +0400

    adding cdk configuration - 2

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { Lambda } from '../lib/lambda-stack';
-import {S3Bucket} from "../lib/s3-stack";
+import { S3Bucket } from '../lib/s3-stack';
 const prodEnv = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
